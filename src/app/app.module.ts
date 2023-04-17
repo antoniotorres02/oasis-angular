4eabaab894326867958cb8a5799fc0887715daac
antoniotorres02 <torresbordonantonio@gmail.com>
2023-04-17 08:15:11 +0100

    Añadidos componentes y estructura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { ShopComponent } from './pages/shop/shop.component';
+import { PrincipalComponent } from './pages/principal/principal.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    ShopComponent,
+    PrincipalComponent,
+    ProfileComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule
