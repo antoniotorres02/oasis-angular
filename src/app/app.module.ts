6abff76e18b7a9f4df5e550e837dd296ff79b807
juanjesus00 <jjsmx24@gmail.com>
2023-04-18 10:28:51 +0100

    añadido ruteo entre página main y login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { MainComponent } from './components/main/main.component';
+import { LoginComponent} from "./pages/login/login.component";
 import { ShopComponent } from './pages/shop/shop.component';
 import { PrincipalComponent } from './pages/principal/principal.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { CartComponent } from './pages/cart/cart.component';
+import {RouterModule, Routes} from "@angular/router";
 
+const appRoute: Routes = [
+  {path: '', redirectTo:'Home', pathMatch:'full'},
+  {path: 'Home', component:PrincipalComponent},
+  {path: 'login', component:LoginComponent}
+
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MainComponent,
     ShopComponent,
     PrincipalComponent,
     ProfileComponent,
-    CartComponent
+    CartComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoute),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
