89baeb88e16face391bf762943d86271a0585826
JoseManuel117 <jmksd117@gmail.com>
2023-04-23 23:55:30 +0100

    migracion completa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,13 @@ import { ShopComponent } from './pages/shop/shop.component';
 import { PrincipalComponent } from './pages/principal/principal.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { CartComponent } from './pages/cart/cart.component';
+import { OrdersInProgressComponent } from './pages/profile/orders-in-progress/orders-in-progress.component';
 
+import { RouterModule } from "@angular/router";
+import { CompletedOrdersComponent } from './pages/profile/completed-orders/completed-orders.component';
+import { WishlistComponent } from './pages/profile/wishlist/wishlist.component';
+import { SQueriesComponent } from './pages/profile/s-queries/s-queries.component';
+import { ProfileDataComponent } from './pages/profile/profile-data/profile-data.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +26,11 @@ import { CartComponent } from './pages/cart/cart.component';
     PrincipalComponent,
     ProfileComponent,
     CartComponent,
+    OrdersInProgressComponent,
+    CompletedOrdersComponent,
+    WishlistComponent,
+    SQueriesComponent,
+    ProfileDataComponent,
   ],
   imports: [
     BrowserModule
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  pedidosCargado: boolean = true;
+  pedidosCompletosCargado: boolean = false;
+  wishlist: boolean = false;
+  soporteCargado: boolean = false;
+  configuracionCargado: boolean = false;
 
+  current: number = 0;
+
+  arrayAux: (&boolean[]) = [this.pedidosCargado, this.pedidosCompletosCargado, this.wishlist, this.soporteCargado, this.configuracionCargado];
+
+  public cargarComponente(currentDisplay: number) {
+    this.arrayAux[this.current] = false;
+    this.arrayAux[currentDisplay] = true;
+    this.current = currentDisplay;
+    console.log(this.arrayAux[currentDisplay]);
+  }
 }
