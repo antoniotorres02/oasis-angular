7389142acf074cf2ff2fb75f310806b94d3019db
NestorManeiro <91387558+NestorManeiro@users.noreply.github.com>
2023-04-24 18:38:40 +0100

    madre mia tt

---
+++ src/app/pages/formempresa/formempresa.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formempresa',
+  templateUrl: './formempresa.component.html',
+  styleUrls: ['./formempresa.component.css']
+})
+export class FormempresaComponent {
+
+}
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {JsService} from "../../service/js.service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  constructor(private js: JsService) {
+    this.js.loadModuleScript('/nestor','LoginValidation.js');
+  }
+
+}
